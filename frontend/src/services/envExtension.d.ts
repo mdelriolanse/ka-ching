913ded741d141dfc35d19a8b4c575f1a6af344c3
cclaireshi <claireshi25@berkeley.edu>
2025-09-20 14:13:34 -0400

    added mongo url: :

--- frontend/src/services/envExtension.d.ts
+++ frontend/src/services/envExtension.d.ts
@@ -1,6 +1,6 @@
 interface ImportMeta {
   readonly env: {
-    readonly VITE_BACKEND_URL?: string;
+    readonly VITE_BACKEND_URL?: "mongodb+srv://ac3337_db_user:cJezTu0qS8oyCyhc@kaching.ucoxmaw.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&appName=kaching";
     readonly VITE_CORE_URL?: string;
     readonly VITE_MCP_URL?: string;
     readonly
